be086d22a1a6deb653faf28bdd532fd69f3da9b8
1916eco <1916eco@gmail.com>
2022-03-03 20:22:56 +0000

    display user Location

--- src/Components/LeafletMap.jsx
+++ src/Components/LeafletMap.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet'
 import chargerData from "../data/chargers.json"
 import ElectricCarCharger from './ElectricCarCharger'
 import SideControlBar from './SideControlBar'
 import WeatherLayers from './WeatherLayers'
 //https://chargepoints.dft.gov.uk/api/retrieve/registry/lat/57.148/long/-2.108/dist/10/format/json/
+import { Marker, Popup } from 'react-leaflet'
 
 function LeafletMap() {
 
 
-  console.log(chargerData)
+  const [latUser, setLatUser] = useState(null);
+  const [lngUser, setLngUser] = useState(null);
+
+
+  
+  navigator.geolocation.getCurrentPosition((position) => {
+    setLatUser(position.coords.latitude);
+    setLngUser(position.coords.longitude);
+  });
+
   return (
     <div >
   <SideControlBar/>
@@ -25,6 +36,13 @@ function LeafletMap() {
 />
   {/* <WeatherLayers type="temp_new"/> */}
     
+  {latUser&& lngUser&&   <Marker position={[latUser,lngUser]}>
+      <Popup>
+        User Location,<br/> Not fully accurate. 
+      </Popup>
+    </Marker>}
+
+
   <ElectricCarCharger/>
 
 </MapContainer>
