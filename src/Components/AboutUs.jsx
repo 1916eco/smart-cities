184e1c37b43c60a5cc76ea46b3255377b01d0c5f
1916eco <1916eco@gmail.com>
2022-04-28 13:03:16 +0100

    Aboutpage change

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div className="container text-center col">
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Abstract</h1>
+      <p>
+        Smart homes have boosted productivity and have had a great impact on the everyday life of people that own smart
+        technologies. Smart Cities Visualisation looks at this theory with a broader scope taking a whole city into the
+        equation and finding out how having smart sensors and available data for the locals to freely view would impact{" "}
+      </p>
+      <h1>Aim</h1>
+      <p>
+        The project aims to bring all the data that is available from government-supplied APIs and globally available
+        data such as the weather and air quality and visualising all the data together with a layering system for the
+        users to be able to pick and choose what type of data they are interested in viewing.
+      </p>
+      <h1>Motivation</h1>
+      <p>
+        Smart home technologies have been of great interest to me as it is fascinating how much help it can bring to a
+        family home with automated actions to switch lights on and off and routine actions such as curtains open at a
+        specific time of the morning. All these technologies are done with smart sensors and taking the same ideas and
+        looking at a bigger scope outside of a home and having all the sensors around a city could be as useful as in
+        the house.
+      </p>
+      <h1>Contact Information</h1>
+      <p>
+        Enrico Simon
+        <br />
+        <a target="_blank" href="https://linkedin.com/in/enricosimon">
+          LinkedIn
+        </a>
+        <br />
+        Email: 1916eco@gmail.com
+      </p>
+    </div>
+  );
 }
 
-export default AboutUs  
+export default AboutUs;
